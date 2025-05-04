42e0e5a03366ac9d0c21916beb51c345ec54a08a
ryosuke1832 <84903480+ryosuke1832@users.noreply.github.com>
2025-05-04 12:47:43 +1000

    Add initial Xcode project

---
+++ SRMY_iOS_app/SRMY_iOS_app/SRMY_iOS_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  SRMY_iOS_appApp.swift
+//  SRMY_iOS_app
+//
+//  Created by user on 2025/05/04.
+//
+
+import SwiftUI
+
+@main
+struct SRMY_iOS_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SRMY_iOS_app/SRMY_iOS_appTests/SRMY_iOS_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  SRMY_iOS_appTests.swift
+//  SRMY_iOS_appTests
+//
+//  Created by user on 2025/05/04.
+//
+
+import Testing
+@testable import SRMY_iOS_app
+
+struct SRMY_iOS_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
