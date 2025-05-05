22325c6d836bc1d6f7058fde6705f2f22d8ae570
ryosuke1832 <84903480+ryosuke1832@users.noreply.github.com>
2025-05-05 20:36:02 +1000

    creat CRUD functions

--- SRMY_iOS_app/SRMY_iOS_app/Models/Habit.swift
+++ SRMY_iOS_app/SRMY_iOS_app/Models/Habit.swift
@@ -5,21 +5,34 @@
 //  Created by user on 2025/05/04.
 //
 
+
 import Foundation
 import SwiftUI
 
-struct Habit: Identifiable, Codable {
+struct Habit: Identifiable, Codable, Equatable {
     let id: UUID
     var name: String
+    var goalDays: Int
+    var startDate: Date
+    var streakCount: Int
     var lastCompletedDate: Date?
     
+
     init(
         id: UUID = UUID(),
         name: String,
+        goalDays: Int,
+        startDate: Date = Date(),
+        streakCount: Int = 0,
         lastCompletedDate: Date? = nil
     ) {
         self.id = id
         self.name = name
+        self.goalDays = goalDays
+        self.startDate = startDate
+        self.streakCount = streakCount
         self.lastCompletedDate = lastCompletedDate
     }
+    
+
 }
