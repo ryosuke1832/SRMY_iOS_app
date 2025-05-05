88701a066d1980f03791a840475ee28ff3261ea6
Stephanie Fernandez <stephanie.j.fernandez@student.uts.edu.au>
2025-05-05 22:12:34 +1000

    drafted UI HabitSettingView

--- SRMY_iOS_app/SRMY_iOS_app/Views/HabitSettingView.swift
+++ SRMY_iOS_app/SRMY_iOS_app/Views/HabitSettingView.swift
@@ -8,14 +8,82 @@
 import SwiftUI
 
 struct HabitSettingView: View {
+    
+    @State private var selection = "daily"
+        let colours = ["daily", "weekly", "fortnightly", "monthly", "quarterly"]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("set your goals")
+                .font(.title)
+            
+            Spacer()
+
+            Text("write a list of daily goals you want to achieve")
+                .font(.title2)
+            
+            TextField("run for 30 minutes", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 16)
+                        .foregroundColor(.accentColor)
+                )
+            TextField("run for 30 minutes", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 16)
+                        .foregroundColor(.accentColor)
+                )
+            TextField("run for 30 minutes", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 16)
+                        .foregroundColor(.accentColor)
+                )
+            
+            Spacer()
+
+            Text("when do you want to check in to your goals?")
+                .font(.title2)
+
+            Menu(selection) {
+                ForEach(colours, id: \.self) { colours in
+                    Button (colours, action: {
+                        selection = colours
+                    })
+                }
+            }
+            .pickerStyle(.menu)
+            .padding(.all, 16)
+            .foregroundStyle(Color.white)
+            .background(RoundedRectangle(cornerRadius: 16).fill(Color.black))
+            
+            Spacer()
+
+            HStack{
+                Button("Back") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    
+                }
+                .padding()
+                .background(RoundedRectangle(cornerRadius: 16).fill(Color.black))
+
+                Button("Next") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                .padding()
+                .background(RoundedRectangle(cornerRadius: 16).fill(Color.black))
+
+            }
+            
         }
         .padding()
+        
     }
+    
+    
 }
 
+#Preview {
+    HabitSettingView()
+}
